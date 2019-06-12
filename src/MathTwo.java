ad5e87c9296bccb8b3d66c9a1e703b1c0919adca
EricGabriel7 <ethegmc@gmail.com>
2019-06-12 13:43:27 -0400

    first commit

---
+++ src/MathTwo.java
@@ -0,0 +1,8 @@
+public class MathTwo {
+    public static void main(String[] args){
+        int mynumber = 2;
+        float myothernumber = 1.7938f;
+        System.out.println(mynumber);
+        System.out.println(myothernumber);
+    }
+}
